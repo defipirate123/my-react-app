1cde518155d224f07104364a197a626bc7bd72a8
Defi Pirate <defipirate123@gmail.com>
2024-10-13 19:41:03 -0500

    machu da

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 function App() {
   return (
     <div>
-      <h1>Hello, React!</h1>
+      <h1>Hello, Sanketh!</h1>
       <p>This is a basic React boilerplate.</p>
     </div>
   );
